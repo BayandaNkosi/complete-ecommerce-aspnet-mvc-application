8e77348350737a6842b3f341ab107ebce7cecf50
BayandaNkosi <162554556+BayandaNkosi@users.noreply.github.com>
2024-07-16 21:17:55 +0200

    Add project files.

---
+++ Umkomaas-eTickets/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Umkomaas_eTickets.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
